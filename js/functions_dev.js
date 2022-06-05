e4a5d0183d19a039e846cf0bc12ab8859ea847c0
User <bouaiche.9@gmail.com>
2022-06-05 17:09:55 +0100

    Generify the project

--- js/functions_dev.js
+++ js/functions_dev.js
@@ -9,6 +9,74 @@ var clientHeight = $(window).height();
 
 var last;
 
+const capitalize = (str) => str.split(" ").map((v) => v[0].toUpperCase() + v.substr(1)).join(" ");
+
+function getAllUrlParams(url) {
+
+  // get query string from url (optional) or window
+  var queryString = decodeURI(url ? url.split('?')[1] : window.location.search.slice(1));
+
+  // we'll store the parameters here
+  var obj = {};
+
+  // if query string exists
+  if (queryString) {
+
+    // stuff after # is not part of query string, so get rid of it
+    queryString = queryString.split('#')[0];
+
+    // split our query string into its component parts
+    var arr = queryString.split('&');
+
+    for (var i = 0; i < arr.length; i++) {
+      // separate the keys and the values
+      var a = arr[i].split('=');
+
+      // set parameter name and value (use 'true' if empty)
+      var paramName = a[0];
+      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+      // (optional) keep case consistent
+      // paramName = paramName.toLowerCase();
+      // if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+      if (paramName.match(/\[(\d+)?\]$/)) {
+
+        // create key if it doesn't exist
+        var key = paramName.replace(/\[(\d+)?\]/, '');
+        if (!obj[key]) obj[key] = [];
+
+        // if it's an indexed array e.g. colors[2]
+        if (paramName.match(/\[\d+\]$/)) {
+          // get the index value and add the entry at the appropriate position
+          var index = /\[(\d+)\]/.exec(paramName)[1];
+          obj[key][index] = paramValue;
+        } else {
+          // otherwise add the value to the end of the array
+          obj[key].push(paramValue);
+        }
+      } else {
+        // we're dealing with a string
+        if (!obj[paramName]) {
+          // if it doesn't exist, create property
+          obj[paramName] = paramValue;
+        } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+          // if property does exist and it's a string, convert it to an array
+          obj[paramName] = [obj[paramName]];
+          obj[paramName].push(paramValue);
+        } else {
+          // otherwise add the property
+          obj[paramName].push(paramValue);
+        }
+      }
+    }
+  }
+
+  return obj;
+}
+
+
 function callWhenNeeded(first, second, callback) {
 	if (callback && first === second) callback.call();
 }
@@ -46,17 +114,17 @@ function timeElapse(date) {
   var current = Date();
   var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
   var days = Math.floor(seconds / (3600 * 24));
-  seconds = seconds % (3600 * 24);
+  seconds %= (3600 * 24);
   var hours = Math.floor(seconds / 3600);
   if (hours < 10) {
     hours = "0" + hours;
   }
-  seconds = seconds % 3600;
+  seconds %= 3600;
   var minutes = Math.floor(seconds / 60);
   if (minutes < 10) {
     minutes = "0" + minutes;
   }
-  seconds = seconds % 60;
+  seconds %= 60;
   if (seconds < 10) {
     seconds = "0" + seconds;
   }
